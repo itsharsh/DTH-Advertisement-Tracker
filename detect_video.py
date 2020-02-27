00093bb545f3f2f36a00f31c3770cdff80e37d7e
Harsh <hkumarsingh142@gmail.com>
2020-02-27 16:37:43 +0530

    CSV Header added, second to frame conversion

--- detect_video.py
+++ detect_video.py
@@ -4,11 +4,11 @@ import csv
 import time
 import numpy as np
 import pandas as pd
-from datetime import timedelta
 from datetime import datetime
+from datetime import timedelta
 
 modelName = "/9_Ads"
-csvFilePath = "/home/harsh/Desktop/"
+csvFilePath = "CSV/"
 modelDir = "/mnt/6C8CA6790B328288/Projects/AI/AdTracker/Model"
 videoPath = "/mnt/6C8CA6790B328288/Projects/AI/AdTracker/Test Videos/"
 
@@ -16,10 +16,10 @@ configPath = modelDir+modelName+modelName+".cfg"
 classesPath = modelDir+modelName+modelName+".names"
 weightsPath = modelDir+modelName+modelName+".weights"
 
-videoName = "20191218-135227.mp4"
-channelName = "Star Sports 1 Hindi"
 adType = "AdTypeUnknown"
 csvFileName = "adtrack.csv"
+videoName = "20191218-135227.mp4"
+channelName = "Star Sports 1 Hindi"
 
 frameH = 416
 frameW = 416
@@ -72,6 +72,7 @@ def captureFrames(path):
     classIndex = [None]*len(classes)
     # classIndex = [[54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 184, 185, 185, 186, 186, 187, 187, 188, 188, 189, 189, 190, 190, 191, 191, 192, 192, 193, 193, 194, 194, 195, 195, 196, 196, 197, 197, 198, 198, 199, 199, 200, 200, 201, 201, 202, 202, 203, 203, 204, 204, 205, 205, 206, 206, 207, 207, 208, 208, 209, 209, 210, 210, 211, 211, 212, 212, 213, 213, 214, 214, 215, 215, 216, 216, 217, 217, 218, 218, 219, 219, 220, 220, 221, 221, 222, 222, 223, 223, 224, 224, 225, 225, 226, 226, 227, 227, 228, 228, 229, 229, 230, 230, 231, 231, 232, 232, 233, 233, 234, 234, 235, 235, 236, 236, 237, 237, 238, 238, 239, 239, 240, 240, 241, 241, 242, 242, 243, 243, 244, 244, 245, 245, 246, 246, 247, 247, 248, 248, 249, 249, 250, 250, 251, 251, 252, 252, 253, 253, 254, 254, 255, 255, 256, 256, 257, 257, 258, 258, 259, 259, 260, 260, 261, 261, 262, 262, 263, 263, 264, 264, 265,
     #                265, 266, 266, 267, 267, 268, 268], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23], None, [184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 239, 240, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268], [156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183], [156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183], [156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182], [156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183], [156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183]]
+    # Detection
     try:
         while True:
             (grabbed, frame) = videoObject.read()
@@ -81,7 +82,7 @@ def captureFrames(path):
             frameTime = timedelta(
                 seconds=frameIndex/frameToRead)
 
-            cv2.putText(frame, str(baseTimestamp+frameTime), (10, 30),
+            cv2.putText(frame, (baseTimestamp+frameTime).strftime("%Y/%m/%d-%H:%M:%S.%f")[:-3], (10, 30),
                         cv2.FONT_HERSHEY_COMPLEX, 0.75, (255, 255, 255), 1)
 
             if W is None or H is None:
@@ -155,19 +156,46 @@ def captureFrames(path):
     except:
         print("Exception while object detection: ", sys.exc_info())
 
+    # Update in CSV DB
     try:
         for i, classList in enumerate(classIndex):
             if classList is not None:
                 classList = getStartEnd(classList)
                 for startEnd in classList:
-                    adStartTime = timedelta(seconds=startEnd[0]/frameToRead)
-                    adEndTime = timedelta(seconds=startEnd[1]/frameToRead)
-                    duration = adEndTime-adStartTime
-                    clipFileName = "{}-{}-{}-{}-{}".format(
-                        videoName.split(".")[0], startEnd[0], startEnd[1], classes[i], channelName)
+                    index = updateDBIndex()
+                    sourceFile = videoName.split(".")[0]
+                    global channelName
+                    global adType
+                    brandName = classes[i]
 
-                    row = [updateDBIndex(), channelName, adType, classes[i], baseTimestamp.date(),
-                           (baseTimestamp+adStartTime).time(), (baseTimestamp+adEndTime).time(), duration, clipFileName]
+                    date = baseTimestamp.date()
+
+                    adFrameStart = startEnd[0]
+                    adFrameEnd = startEnd[1]
+                    adClipStart = timedelta(
+                        seconds=adFrameStart/frameToRead)
+                    adClipEnd = timedelta(
+                        seconds=adFrameEnd/frameToRead)
+                    duration = (adClipEnd-adClipStart).total_seconds()
+
+                    adStart = ((baseTimestamp+adClipStart).time()
+                               ).strftime("%H:%M:%S.%f")[:-3]
+                    adEnd = ((baseTimestamp+adClipEnd).time()
+                             ).strftime("%H:%M:%S.%f")[:-3]
+
+                    clipFileName = "{}-{}-{}-{}-{}".format(
+                        sourceFile, adFrameStart, adFrameEnd,
+                        classes[i], channelName)
+
+                    header = ["DB Index", "Channel Name", "Type of Ad", "Brand Name",
+                              "Date", "Ad Start", "Ad End", "Duration", "Clip File Name",
+                              "Source File", "Ad Frame Start", "Ad Frame End"]
+                    row = [index, channelName, adType, brandName,
+                           date, adStart, adEnd, duration, clipFileName,
+                           sourceFile, adFrameStart, adFrameEnd]
+
+                    if index == 1:
+                        updateCSV(header)
                     updateCSV(row)
     except:
         print("Exception while updating DB: ", sys.exc_info())
@@ -182,9 +210,8 @@ def getStartEnd(nums):
 
 def updateDBIndex():
     try:
-        df = pd.read_csv(csvFilePath+csvFileName, header=None, usecols=[0])
-        # add 1 for change start index to 1, and add 1 for counter
-        return int(df.idxmax())+2
+        df = pd.read_csv(csvFilePath+csvFileName)
+        return df["DB Index"].max()+1  # add 1 for counter
 
     except pd.errors.EmptyDataError:
         print("Empty CSV File")
