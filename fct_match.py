afc4166c003de3dad0bdae624158a7a5b2333e90
Harsh <hkumarsingh142@gmail.com>
2020-03-30 16:59:18 +0530

    Formatting Added

--- fct_match.py
+++ fct_match.py
@@ -9,42 +9,49 @@ Original file is located at
 
 import cv2
 from skimage.measure import compare_ssim as ssim
-cap=cv2.VideoCapture("ad07.mp4")
-cap1=cv2.VideoCapture("ad13.mp4")
-total_frame=int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-total_frame1=int(cap1.get(cv2.CAP_PROP_FRAME_COUNT))
+
+videoPath = "D:\Office\Backup\Projects Data\AI\AdTracker\DTH"
+originalFileName = "20191218-135227.mp4"
+clipFileName = "20191218-135227-2137-2152-1Mg-Star Sports 1 Hindi.mp4"
+
+cap = cv2.VideoCapture(originalFileName)
+cap1 = cv2.VideoCapture(clipFileName)
+
+total_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+total_frame1 = int(cap1.get(cv2.CAP_PROP_FRAME_COUNT))
 print(total_frame, total_frame1)
-m=0
-frames_list=[]
+m = 0
+frames_list = []
 
 
-fps=int(cap.get(cv2.CAP_PROP_FPS))
+fps = int(cap.get(cv2.CAP_PROP_FPS))
 
 print(fps)
 while cap.isOpened:
-    
-      for i in range (total_frame):
-          cap.set(cv2.CAP_PROP_POS_FRAMES,i)
-          ret,frame=cap.read()
-          for j in range (total_frame1):
-              cap1.set(cv2.CAP_PROP_POS_FRAMES,j)
-              ret1,frame1=cap1.read()
-              diff=cv2.subtract(frame,frame1)
-              b, g, r =cv2.split(diff)
-              s = ssim(frame,frame1, multichannel = True)
-              if (cv2.countNonZero(b)==0 and cv2.countNonZero(g)==0 and cv2.countNonZero(r)==0) or s>=.97:
-                  print("matched",cap.get(cv2.CAP_PROP_POS_FRAMES), "--",cap1.get(cv2.CAP_PROP_POS_FRAMES),"ssim=",s)
-                  
-                  frames_list.append(cap.get(cv2.CAP_PROP_POS_FRAMES))
-                  m+=1
-                  
-                  break
-              else:
-                
-                print("not matched", cap.get(cv2.CAP_PROP_POS_FRAMES),"--",cap1.get(cv2.CAP_PROP_POS_FRAMES),"ssim=",s)
-         
-      print("total frames matched=",m)
-      
-      break     
-print(frames_list)
 
+    for i in range(total_frame):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, i)
+        ret, frame = cap.read()
+        for j in range(total_frame1):
+            cap1.set(cv2.CAP_PROP_POS_FRAMES, j)
+            ret1, frame1 = cap1.read()
+            diff = cv2.subtract(frame, frame1)
+            b, g, r = cv2.split(diff)
+            s = ssim(frame, frame1, multichannel=True)
+            if (cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0) or s >= .97:
+                print("matched", cap.get(cv2.CAP_PROP_POS_FRAMES), "--",
+                      cap1.get(cv2.CAP_PROP_POS_FRAMES), "ssim=", s)
+
+                frames_list.append(cap.get(cv2.CAP_PROP_POS_FRAMES))
+                m += 1
+
+                break
+            else:
+
+                print("not matched", cap.get(cv2.CAP_PROP_POS_FRAMES),
+                      "--", cap1.get(cv2.CAP_PROP_POS_FRAMES), "ssim=", s)
+
+    print("total frames matched=", m)
+
+    break
+print(frames_list)
