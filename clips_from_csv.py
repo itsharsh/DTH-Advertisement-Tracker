d0f3c3f9642d5fe64771829c4d981ce28debb3b8
Hulk Buster <vivek06sharma11@gmail.com>
2020-04-04 12:32:42 +0530

    ffmpeg line

--- clips_from_csv.py
+++ clips_from_csv.py
@@ -1,16 +1,16 @@
 import os
 import numpy as np
 import pandas as pd
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 channelNameList = ["Star Sports 1", "Star Sports 1 Hindi"]
-workingDirectory = "/mnt/6C8CA6790B328288/Projects/AI/AdTracker/DTH"
-videoDataPath = workingDirectory+"\Original"
-outputDataPath = workingDirectory+"\Ad Clips"
-dataFilePath = "CSV/adtrack.csv"
-
-makeDirectoryCommand = "mkdir \""+outputDataPath+"\""
+workingDirectory = "/home/vivek/Test"
+videoDataPath = workingDirectory+"/Original"
+outputDataPath = workingDirectory+"/Ad Clips"
+dataFilePath = "/home/vivek/Test/CSV/20200117-213035-17000-1f.csv"
+makeDirectoryCommand = "mkdir -p \""+outputDataPath+"\""
 print(makeDirectoryCommand)
-# os.system(makeDirectoryCommand)
+os.system(makeDirectoryCommand)
 
 for channelName in channelNameList:
     print("Cutting clips in "+channelName)
@@ -28,38 +28,45 @@ for channelName in channelNameList:
     dataset = dataset.reset_index()
     dataset = dataset.drop("index", axis=1)
 
-    fileNameArray = np.array(dataset['Time Stamp'])
+    fileNameArray = np.array(dataset['Source File'])
     channelNameArray = np.array(dataset['Channel Name'])
-    adBrandNameArray = np.array(dataset['Ad Brand Name'])
+    adBrandNameArray = np.array(dataset['Brand Name'])
     print(fileNameArray)
     fileTimeStampArray = pd.to_datetime(
-        dataset['Time Stamp'], errors='coerce', format="%Y%m%d-%H%M%S")
+        dataset['Source File'], errors='coerce', format="%Y%m%d-%H%M%S")
     print(fileTimeStampArray)
 
     adStartTimeArray = pd.to_timedelta(dataset['Ad Start'])
     adEndTimeArray = pd.to_timedelta(dataset['Ad End'])
     adDurationArray = adEndTimeArray-adStartTimeArray
+    adClipFileName=np.array(dataset['Clip File Name'])
+    adStartTime=np.array(dataset['Ad Frame Start'])
+    adEndTime=np.array(dataset['Ad Frame End'])
     print(dataset)
 
     fileIndex = 0
     while(fileIndex < fileNameArray.size):
         print("Cutting file: "+str(fileIndex+1) +
               " "+adBrandNameArray[fileIndex])
-        makeDirectoryPath = outputDataPath+"\\" + \
-            adBrandNameArray[fileIndex]+"\\" + channelNameArray[fileIndex]
+        makeDirectoryPath = outputDataPath+"/" + \
+            adBrandNameArray[fileIndex]+"/" + channelNameArray[fileIndex]
         print(makeDirectoryPath)
-        # os.system("mkdir \""+makeDirectoryPath+"\"")
-        terminalCommand = "ffmpeg -n -i \""+videoDataPath+"\\"+channelName+"\\" + \
-            fileNameArray[fileIndex]+".mp4\""+" -ss " + \
-            str(adStartTimeArray[fileIndex].total_seconds()) + " -t " + \
-            str(adDurationArray[fileIndex].total_seconds()) + " \"" + \
-            makeDirectoryPath+"\\"+fileNameArray[fileIndex] + \
-            "-" + str(int(adStartTimeArray[fileIndex].total_seconds())) + \
-            "-" + str(int(adEndTimeArray[fileIndex].total_seconds())) + \
-            "-" + adBrandNameArray[fileIndex]+"-"+channelName+".mp4\""
+        os.system("mkdir -p \""+makeDirectoryPath+"\"")
+        sourceFile=videoDataPath+"/"+str(fileNameArray[fileIndex])+".mp4"
+        outPutFile=makeDirectoryPath+"/"+str(adClipFileName[fileIndex])+".mp4"
+        print(sourceFile)
+        print(outPutFile)
+        print(adStartTime[fileIndex]-1,adEndTime[fileIndex]+1)
+        ffmpeg_extract_subclip(sourceFile,adStartTime[fileIndex]*0.04,adEndTime[fileIndex]*0.04,outPutFile)
+        #terminalCommand = "ffmpeg -n -i \""+videoDataPath+"/" + \
+        #    str(fileNameArray[fileIndex])+".mp4\""+" -ss " + \
+        #    str(adStartTime[fileIndex]) + " -t " + \
+        #    str(adDurationArray[fileIndex].total_seconds()+1) + " \"" + \
+        #        makeDirectoryPath+"/"+adClipFileName[fileIndex]+".mp4\""
 
-        print(terminalCommand)
-        # print(os.popen(terminalCommand).read())
+
+        #print(terminalCommand)
+        #print(os.popen(terminalCommand).read())
         fileIndex += 1
 
     print(channelName+" Completed")
