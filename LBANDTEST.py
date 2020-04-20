2130d8933718ec77e9a8f6e5057a7df2cbc71c6f
AI-ctrl <54680798+AI-ctrl@users.noreply.github.com>
2020-04-20 13:37:32 +0530

    Few changes

--- LBANDTEST.py
+++ LBANDTEST.py
@@ -4,27 +4,55 @@ from time import process_time
 
 import detect_db
 import detect_branding
+import path_config
+
+videoPath = path_config.originalVideoDir
+tempPath = path_config.brandNonFCTFilePath
+
+videoPath = r"C:\Users\Hp\Desktop\testvid"
+tempPath = r"C:\Users\Hp\Desktop\Data\Merinolam"
+
+
+max_list1 = []
+max_list2 = []
+max_list3 = []
+aston_frame_list = []
+LBand_frame_list = []
+L_frame_list = []
+
+miscInfo = {
+    "channelName": "Star Sports 1",
+    "videoName": "20200117-213035.mp4",
+    "adType": "Non-FCT",
+    "videoFPS": 25,
+    "frameToRead": 1  # read every nth frame
+}
+baseTimestamp = detect_branding.getTimestampFromVideofile(
+    miscInfo["videoName"])
+detectionInfo = {"classIndex": L_frame_list, "classes": "merinolam", "baseTimestamp": baseTimestamp,
+                 "frameDimensions": (256, 256)}
+
 
 # LBand1
 
-temp1 = r"C:\Users\Hp\Pictures\LBand1.jpg"
+temp1 = r"C:\Users\Hp\Desktop\Data\Merinolam\Merinolam_LBand1_2_cropped.jpeg"
 L1Band = cv2.imread(temp1, cv2.IMREAD_GRAYSCALE)
 print(L1Band.shape)
 w1, h1 = L1Band.shape[::-1]
 
 # LBand2
 
-temp2 = r"C:\Users\Hp\Pictures\LBand2jpg.jpg"
+temp2 = r"C:\Users\Hp\Desktop\Data\Merinolam\Merinolam_LBand2_2_cropped.jpeg"
 L2Band = cv2.imread(temp2, cv2.IMREAD_GRAYSCALE)
 print(L2Band.shape)
 w2, h2 = L2Band.shape[::-1]
 
 # astonBand
 
-temp3 = r"C:\Users\Hp\Desktop\aslam ppt\Merinolam_AstonBand_1n.jpeg"
+temp3 = r"C:\Users\Hp\Desktop\Data\Merinolam\Merinolam_AstonBand_1_cropped.jpeg"
 astonBand = cv2.imread(temp3, cv2.IMREAD_GRAYSCALE)
 print(astonBand.shape)
-astonBand = cv2.resize(astonBand, (720, 100), interpolation=cv2.INTER_NEAREST)
+#astonBand = cv2.resize(astonBand, (720, 100), interpolation=cv2.INTER_NEAREST)
 w3, h3 = astonBand.shape[::-1]
 # methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
 #          'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
@@ -53,8 +81,8 @@ detectionInfo = {"classIndex": L_frame_list, "classes": "merinolam", "baseTimest
 # videoFile
 
 #videoFile = r"C:\Users\Hp\Desktop\Data\20200117-125957-3400-3420-Merinolam-Star Sports 1.mp4"
-#videoFile = r"C:\Users\Hp\Desktop\20200117-131854.mp4"
-videoFile = r"C:\Users\Hp\Desktop\20200117-131854ss1.mp4"
+videoFile = r"C:\Users\Hp\Desktop\20200117-131854.mp4"
+#videoFile = r"C:\Users\Hp\Desktop\testvid\20200117-131854ss1.mp4"
 #videoFile = r"C:\Users\Hp\Documents\Bandicut\20200117-131854TestLBand.mp4"
 #videoFile = r"C: \Users\Hp\Documents\Bandicut\20200117-131854ss1.mp4"
 
@@ -72,15 +100,15 @@ while video.isOpened:
     ret, frame = video.read()
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frameNo = video.get(cv2.CAP_PROP_POS_FRAMES)
-    print(frame_gray.shape)
-    print(frameNo)
-    print(L1Band.shape)
-    print(L2Band.shape)
+    # print(frame_gray.shape)
+    # print(frameNo)
+    # print(L1Band.shape)
+    # print(L2Band.shape)
 
     # Apply template Matching for L BAND test
 
-    res1 = cv2.matchTemplate(frame_gray, L1Band, cv2.TM_CCOEFF)
-    res2 = cv2.matchTemplate(frame_gray, L2Band, cv2.TM_CCOEFF)
+    res1 = cv2.matchTemplate(frame_gray, L1Band, cv2.TM_CCOEFF_NORMED)
+    res2 = cv2.matchTemplate(frame_gray, L2Band, cv2.TM_CCOEFF_NORMED)
     min_val1, max_val1, min_loc1, max_loc1 = cv2.minMaxLoc(res1)
     max_list1.append(max_val1)
     max_list1.append(max_val1)
@@ -94,7 +122,7 @@ while video.isOpened:
     bottom_right2 = (top_left2[0]+w2, top_left2[1]+h2)
 # Apply templateMAching for aston band
 
-    res3 = cv2.matchTemplate(frame_gray, astonBand, cv2.TM_CCOEFF)
+    res3 = cv2.matchTemplate(frame_gray, astonBand, cv2.TM_CCOEFF_NORMED)
     min_val3, max_val3, min_loc3, max_loc3 = cv2.minMaxLoc(res3)
     max_list3.append(max_val3)
     max_list3.append(max_val3)
@@ -102,18 +130,24 @@ while video.isOpened:
     top_left3 = max_loc3
     bottom_right3 = (top_left3[0] + w3, top_left3[1] + h3)
     #print(max_val1,   max_val2)
-    threshold1 = 10000000.0
-    threshold2 = 110000000.0
-    threshold3 = 170000000.0
+#    threshold1 = 11000000.0
+ #   threshold2 = 110000000.0
+  #  threshold3 = 170000000.0
+    threshold1 = .55
+    threshold2 = .55
+    threshold3 = .55
+
     #list1 = []
     if max_val1 > threshold1 and max_val2 > threshold2:
         cv2.rectangle(frame, top_left1, bottom_right1, (0, 255, 255), 2)
         cv2.rectangle(frame, top_left2, bottom_right2, (0, 255, 255), 2)
+        print(max_val1,   max_val2)
         list1.append(frameNo)
         aston_frame_list.append(frameNo)
         print("LBand Detected")
     elif max_val3 > threshold3:
         cv2.rectangle(frame, top_left3, bottom_right3, (255, 255, 255), 4)
+        print(max_val3)
         list1.append(frameNo)
         aston_frame_list.append(frameNo)
         print("aston band detected")
@@ -134,7 +168,7 @@ stop = process_time()
 print("time for 1hr video", stop-start)
 print(L_frame_list)
 # print(aston_frame_list)
-#print(max_list1, max_list2,  max_list3)
+print(max_list1, max_list2,  max_list3)
 # print(detect_db.getStartEnd(aston_frame_list))
 
 # print([detect_db.getStartEnd(L_frame_list)])
